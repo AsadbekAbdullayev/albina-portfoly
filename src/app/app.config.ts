820b14285b65f05762e5e3f9674af41269c9e7f9
Asadbek <71205691+AsadbekAbdullayev@users.noreply.github.com>
2025-09-10 09:52:30 +0500

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,17 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { providePrimeNG } from 'primeng/config';
-import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideAnimationsAsync(),
-    providePrimeNG({
-      theme: {
-        preset: null,
-      },
-    }),
     provideRouter(routes),
+    providePrimeNG({
+      theme: { preset: null },
+    }),
   ],
 };
